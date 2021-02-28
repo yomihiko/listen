5d4d0a9fa54c56cbe54d79faf864ae98a233ae61
yomihiko <ayanium777@gmail.com>
2021-02-28 17:34:48 +0900

    if文

---
+++ chapter09/if.py
@@ -0,0 +1,32 @@
+san = 50
+if san == 50:
+    print(san)
+
+names = ['田中', '吉田', '佐藤']
+if '田中' in names:
+    print('田中さん')
+else:
+    print("み")
+    print('む')
+print('も')
+
+color = 'BLUE'
+if color == 'RED':
+    print('赤')
+elif color == 'BLUE':
+    print('青')
+
+numbers = {1, 2, 4, 5, 6}
+if numbers:
+    print("おおお")
+# falseと評価されるObject
+# 空文字 '' 空タプル () 空リスト [] 空辞書 {} 空セット set() 整数0 浮動小数点数の0.0 False None
+sora = set()
+if sora:
+    print('boi')
+else :
+    print('moi')
+# s
+# s
+# s
+#
